3e3e4de8208b9d235fb8d8e02644eecd6e09ac27
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2023-04-07 18:20:27 -0300

    Exemplo procedural do programa sequencial anterior

---
+++ Capitulo-I/addEmUp.php
@@ -0,0 +1,16 @@
+<?php
+
+//$firstNumber = 20;
+//$secondNUmber = 40;
+//$total = $firstNumber + $secondNumber;
+//echo $total;
+
+
+function addEmUpp($first, $second){
+
+    $total=$first + $second;
+    echo $total;
+}
+
+addEmUpp(20,40);
+?>
